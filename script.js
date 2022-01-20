e86666caf97f925fd91ff50d50eae5be9c1aa2ff
kdz99 <77607588+kdz99@users.noreply.github.com>
2022-01-20 13:06:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 (function(){
 
+	
 	"use strict";
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
 	const CLOCK_ORIGIN = canvas.width/2;
 	let clock = {
 
+		
 		tick: Math.PI/6/5,
 		radius: (canvas.width / 2) - 25,
 		sec: 0,
